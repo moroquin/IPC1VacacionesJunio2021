3513e22bcfaa628ef2df4657e868c0adb71c99c1
Oliver Sierra Pac <sierra.oe@gmail.com>
2021-06-11 17:43:46 -0600

    Burbuja.java agregue contador operaciones e intercambio

--- src/Burbuja.java
+++ src/Burbuja.java
@@ -8,21 +8,23 @@ public class Burbuja {
     }
 
     public Burbuja(){
-        int[] matriz = new int[7];
+        int[] matriz = {1,7,4,3,8,9,6,4,0,2,5}; //new int[7];
         
-        llenarMatriz(matriz);
+        //llenarMatriz(matriz);
         imprimirMatriz(matriz);
         System.out.println("\n\nOrdenando . . . \n\n");
         ordenBurbuja(matriz);
         System.out.println("\n\nFin Ordenando . . . \n\n");
         imprimirMatriz(matriz);
-
     }
 
     public void ordenBurbuja(int[] array){
+        int operaciones = 0;
+        int intercambio = 0;
         //i<array.length   =   i <= (array.length-1)
         for (int i = 1; i < array.length; i++) {
             for (int j = 0; j < array.length-i; j++) {
+                operaciones++;
                 if (array[j]> array[j+1]){
                     /*
                     variable1  = 1
@@ -32,6 +34,7 @@ public class Burbuja {
                         variable1 = variable2 = 2
                         variable2 = temporal = 1
                     */
+                    intercambio++;
                     int tmp = array[j];
                     array[j] = array[j+1];
                     array[j+1] = tmp;
@@ -39,6 +42,8 @@ public class Burbuja {
                 
             }
         }
+
+        System.out.println("Operaciones: "+operaciones + " Intercambios"+intercambio);
     }
 
     public void imprimirMatriz(int[] array){
