4f56c903a7e3d94f9028cb521acd05998c87301a
Oliver Sierra Pac <sierra.oe@gmail.com>
2021-06-11 17:38:25 -0600

    Implmente metodo ordenamiento burbuja en el archivo burbuja.java

--- src/Burbuja.java
+++ src/Burbuja.java
@@ -13,13 +13,29 @@ public class Burbuja {
         llenarMatriz(matriz);
         imprimirMatriz(matriz);
         System.out.println("\n\nOrdenando . . . \n\n");
+        ordenBurbuja(matriz);
+        System.out.println("\n\nFin Ordenando . . . \n\n");
+        imprimirMatriz(matriz);
 
     }
 
     public void ordenBurbuja(int[] array){
         //i<array.length   =   i <= (array.length-1)
-        for (int i = 1; i <= (array.length-1); i++) {
-            for (int j = 0; j < array.length; j++) {
+        for (int i = 1; i < array.length; i++) {
+            for (int j = 0; j < array.length-i; j++) {
+                if (array[j]> array[j+1]){
+                    /*
+                    variable1  = 1
+                    variable2 = 2
+
+                        temporal = variable1 = 1
+                        variable1 = variable2 = 2
+                        variable2 = temporal = 1
+                    */
+                    int tmp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = tmp;
+                }
                 
             }
         }
